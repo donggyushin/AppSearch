64d9cd48fb3a80aaa7e1ece02d8452afef02a17a
신동규 <donggyu9410@gmail.com>
2024-03-30 16:43:53 +0900

    SearchController 붙임

--- AppSearch/View/SearchViewController.swift
+++ AppSearch/View/SearchViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-final class SearchViewController: UISearchController {
+final class SearchViewController: UIViewController {
+    private let searchController = UISearchController(searchResultsController: nil)
+    private let viewModel = SearchViewModel(appRepository: AppRepositoryDomain.shared)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+//        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Apps"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
         configUI()
     }
     
     private func configUI() {
-        
+        view.backgroundColor = .systemBackground
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        viewModel.searchQueryUpdated(query: searchController.searchBar.text ?? "")
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        viewModel.tapEnter()
     }
 }
