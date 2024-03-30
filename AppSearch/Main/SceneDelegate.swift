b5bb6480dfcb2bbfe9f113c81239d0a177c5a8d7
donggyu <donggyu_00@naver.com>
2024-03-30 13:39:29 +0900

    App Setting (#3)
    * delete ui tests
    
    * add SearchViewController
    
    * delete ViewController
    
    * delete Main storyboard
    
    * Packaging
    
    * Using UISearchController

---
+++ AppSearch/Main/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  AppSearch
+//
+//  Created by 신동규 on 3/30/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = .init(windowScene: scene)
+        window?.rootViewController = SearchViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
+
