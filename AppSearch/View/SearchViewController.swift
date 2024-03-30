1eb7a03fa23927a7b0a16b9dfad737f5f24940c9
신동규 <donggyu9410@gmail.com>
2024-03-30 13:32:21 +0900

    add SearchViewController

---
+++ AppSearch/View/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  AppSearch
+//
+//  Created by 신동규 on 3/30/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+    }
+    
+    private func configUI() {
+        view.backgroundColor = .red
+    }
+}
