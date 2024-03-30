dc382fe4ab246aa4b561abb38f548e5611339524
donggyu <donggyu_00@naver.com>
2024-03-30 13:39:29 +0900

    App Setting (#3)
    * delete ui tests
    
    * add SearchViewController
    
    * delete ViewController
    
    * delete Main storyboard
    
    * Packaging
    
    * Using UISearchController

---
+++ AppSearch/View/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  AppSearch
+//
+//  Created by 신동규 on 3/30/24.
+//
+
+import UIKit
+
+class SearchViewController: UISearchController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+    }
+    
+    private func configUI() {
+        
+    }
+}
