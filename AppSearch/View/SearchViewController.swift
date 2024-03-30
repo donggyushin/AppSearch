c6b8d8f6c1d905b3a2c5eaf49d069f4e869e8390
신동규 <donggyu9410@gmail.com>
2024-03-30 16:14:21 +0900

    add viewmodel

--- AppSearch/View/SearchViewController.swift
+++ AppSearch/View/SearchViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SearchViewController: UISearchController {
+final class SearchViewController: UISearchController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configUI()
---
+++ AppSearch/View/SearchViewModel.swift
@@ -0,0 +1,63 @@
+//
+//  SearchViewModel.swift
+//  AppSearch
+//
+//  Created by 신동규 on 3/30/24.
+//
+
+import Combine
+
+final class SearchViewModel {
+    private let appRepository: AppRepository
+    private var cancellables: Set<AnyCancellable> = []
+    
+    @Published private(set) var apps: [App] = []
+    @Published private(set) var loading = false
+    @Published private(set) var searchQuery: String = ""
+    @Published private(set) var searchQueryHistories: [String] = []
+    
+    @Published private var allSearchQueryHistories: [String] = []
+    
+    init(appRepository: AppRepository) {
+        self.appRepository = appRepository
+        
+        allSearchQueryHistories = (try? appRepository.getSearchHistory()) ?? []
+        
+        bind()
+    }
+    
+    func searchQueryUpdated(query: String) {
+        searchQuery = query
+    }
+    
+    func tapEnter() {
+        Task {
+            do {
+                guard loading == false else { return }
+                loading = true
+                apps = try await appRepository.get(query: searchQuery)
+                loading = false
+            } catch {
+                loading = false
+            }
+            
+        }
+    }
+    
+    func setSearchQueryHistories(searchQuery: String, allSearchQueryHistories: [String]) -> [String] {
+        guard searchQuery.isEmpty == false else {
+            return allSearchQueryHistories
+        }
+        
+        return allSearchQueryHistories.filter({ $0.lowercased().contains(searchQuery.lowercased()) })
+    }
+    
+    private func bind() {
+        $searchQuery
+            .combineLatest($allSearchQueryHistories)
+            .sink { searchQuery, allSearchQueryHistories in
+                self.searchQueryHistories = self.setSearchQueryHistories(searchQuery: searchQuery, allSearchQueryHistories: allSearchQueryHistories)
+            }
+            .store(in: &cancellables)
+    }
+}
