cd9fe1cdfbc9357b9171020061addb3256a27f78
신동규 <donggyu9410@gmail.com>
2024-03-30 22:57:23 +0900

    header 추가

--- AppSearch/View/Detail/DetailViewController.swift
+++ AppSearch/View/Detail/DetailViewController.swift
@@ -9,7 +9,26 @@ import UIKit
 
 final class DetailViewController: UIViewController {
     
+    private let app: App
+    
+    private lazy var scrollView = UIScrollView()
+    private lazy var contentsView = UIView()
+    
+    private lazy var headerView: HeaderView = {
+        let view = HeaderView()
+        view.configUI(appId: app.trackId, imageUrl: app.artworkUrl512, title: app.trackName, genres: app.genres)
+        view.delegate = self
+        return view
+    }()
+    
+    private lazy var verticalStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [headerView, UIView()])
+        view.axis = .vertical
+        return view
+    }()
+    
     init(app: App) {
+        self.app = app
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -24,5 +43,28 @@ final class DetailViewController: UIViewController {
     
     private func configUI() {
         view.backgroundColor = .systemBackground
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        scrollView.addSubview(contentsView)
+        contentsView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.width.equalTo(view.snp.width)
+        }
+        
+        contentsView.addSubview(verticalStackView)
+        verticalStackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension DetailViewController: DetailHeaderDelegate {
+    func tapDownloadButton(appId: Int) {
+        coordinator.openAppStore(appId: appId)
+        print(appId)
     }
 }
